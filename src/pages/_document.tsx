b65291bfb0b9021ae0527c4bcb5869aa7a23d8c5
anamerasul <anamerasul@gmail.com>
2022-03-15 17:50:11 +0600

    all update

---
+++ src/pages/_document.tsx
@@ -0,0 +1,33 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    return Document.getInitialProps(ctx);
+  }
+  render() {
+    return (
+      <Html lang="en-US">
+        <Head>
+         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
