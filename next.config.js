98b3b20054e8dcfafba08f23266cf474233b517a
MD ANAM E RASUL CHOWDHURAY <46762842+anamerasul@users.noreply.github.com>
2022-03-15 19:25:59 +0600

    update

--- next.config.js
+++ next.config.js
@@ -10,17 +10,6 @@ module.exports = withPWA({
     disable: process.env.NODE_ENV === 'development',
     runtimeCaching,
   },
-  async redirects(){
-    return[
-      {
-        source:'/',
-        destination:'/',
-        permanent:false,
-      },
-    
-
-    ];
-  },
   images: {
     domains: [
       'kitstarter.io',
